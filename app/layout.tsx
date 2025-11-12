c2e19bc16f0ac8768f8b0c1e0cbc11f30ab7c68a
bbs <ppg63275@gmail.com>
2025-11-12 21:21:28 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Nova Notifier -- discord.gg/novanotifier',
+  description: 'Created with Love by Xynnn',
+  generator: 'novanotifier.space',
   icons: {
     icon: [
       {
